44263af83ab17528acfedf8573263c77445a9b69
Muhammad Khaidar Royani <mkhaidar168@gmail.com>
2024-05-01 12:32:31 +0700

    Read data todo

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,13 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index(){
-        return view('todo.index');
+        $todos = Todo::where('user_id', auth()->user()->id)
+
+            ->orderBy('is_complete', 'asc')
+
+            ->orderBy('created_at', 'desc')
+
+            ->get();
+
+        $todosCompleted = Todo::where('user_id', auth()->user()->id)
+            ->where('is_complete', true)
+            ->count();
+
+        return view('todo.index', compact('todos', 'todosCompleted'));
     }
 
     public function create(){
